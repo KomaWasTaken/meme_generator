d53111530b35f699975cef5902208b18b2b226b2
Scalou21 <jerome.rouchy@hotmail.fr>
2018-06-14 16:38:28 +0200

    ajout modale, à faire : ecriture image php, bon placement modale

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+  });
 
 var loadFile = function(event) {
     var output = document.getElementById('output');
