7755dd722ecee41e02905ef26b9b2ecbf7228e94
loanpasionek <l.pasionek@gmail.com>
2018-06-20 16:15:09 +0200

    Reorga des fonctions, affichage des memes generes via upload

--- app/controller/IndexController.php
+++ app/controller/IndexController.php
@@ -2,45 +2,47 @@
 
 class IndexController extends Controller {
 
-    public function verifUpload(){
+    public function display() {
 
-    if(isset($_FILES["fileToUpload"]))  {      
-        $uploadOk = 1;
-        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-
-                if($check !== false) {
-                    echo "File is an image - " . $check["mime"] . ".";
-                    $uploadOk = 1;
-                } else {
-                    echo "File is not an image.";
+    
+    
+        if(isset($_FILES["fileToUpload"]))  {      
+            $uploadOk = 1;
+            $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    
+                    if($check !== false) {
+                        echo "File is an image - " . $check["mime"] . ".";
+                        $uploadOk = 1;
+                        $upload = Gallery::getUploads();
+                    } else {
+                        echo "File is not an image.";
+                        $uploadOk = 0;
+                    }
+                
+                // Check file size
+                if ($_FILES["fileToUpload"]["size"] > 500000) {
+                    echo "Sorry, your file is too large.";
                     $uploadOk = 0;
                 }
-            
-            // Check file size
-            if ($_FILES["fileToUpload"]["size"] > 500000) {
-                echo "Sorry, your file is too large.";
-                $uploadOk = 0;
+                // Allow certain file formats
+    
+                // Check if $uploadOk is set to 0 by an error
+                if ($uploadOk == 0) {
+                    echo "Sorry, your file was not uploaded.";
+                // if everything is ok, try to upload file
+                }         
             }
-            // Allow certain file formats
+            
+        
 
-            // Check if $uploadOk is set to 0 by an error
-            if ($uploadOk == 0) {
-                echo "Sorry, your file was not uploaded.";
-            // if everything is ok, try to upload file
-            }         
-        }
-    }
-
-    public function display() {
-        $memes = Gallery::getMemes();
-        $verif = IndexController::verifUpload();
-        $create = Gallery::createImages();
-        $insert = Gallery::getUploads();
+        $memes = Gallery::getMemes();        
+        $displayUrl = Gallery::displayUrl();
 
         $template = $this->twig->loadTemplate('/Index/display.html.twig');
         echo $template->render(array(
             'memes'  => $memes,
-            'verif' => $verif
+            'url' => $displayUrl,
+            'upload' => $upload
         ));
 
    }
